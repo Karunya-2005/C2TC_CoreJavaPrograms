8ce1414b455ea61eca10dbfb05ca2119ccb5b12d
ACT-C1-25 <ACT-C1-25@LONE-WOLF>
2025-06-12 10:23:43 +0530

    This project contains my core java practical programs.

---
+++ c2tc_CoreJavaPrograms/src/day1/SampleJava.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
