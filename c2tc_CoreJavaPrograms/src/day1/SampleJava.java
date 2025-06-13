9dcdcf03097d91012e1212cbca631c9a37d60845
ACT-C1-25 <ACT-C1-25@LONE-WOLF>
2025-06-13 09:06:02 +0530

    This program contains addition of two numbers.

--- c2tc_CoreJavaPrograms/src/day1/SampleJava.java
+++ c2tc_CoreJavaPrograms/src/day1/SampleJava.java
@@ -1,10 +1,18 @@
 package day1;
-
+import java.util.Scanner;
 public class SampleJava {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to java program");
-		// TODO Auto-generated method stub
+		int a,b,c;
+		
+		Scanner sc ;
+		sc = new Scanner(System.in);
+		System.out.println("Enter a: ");
+		a=sc.nextInt();
+		System.out.println("Enter b: ");
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("Addition of a= "+a+"and b="+b+"is"+c);
 
 	}
 
