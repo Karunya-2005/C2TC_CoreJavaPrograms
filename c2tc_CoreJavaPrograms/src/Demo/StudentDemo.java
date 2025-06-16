bdb928c9974f5289287039fedb3e7756cc826c27
ACT-C1-25 <ACT-C1-25@LONE-WOLF>
2025-06-16 13:56:40 +0530

    These are the programs contains constructor demo

---
+++ c2tc_CoreJavaPrograms/src/Demo/StudentDemo.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class StudentDemo {
+	public static void main(String args[]) {
+		Student student=new Student();
+		student.setId(101);
+		student.setName("Max");
+		student.setAge(20);
+		System.out.println(student);
+		
+		Student student1=new Student();
+		student1.setId(102);
+		student1.setName("Teo");
+		student1.setAge(20);
+		System.out.println(student1                                                     );
+		
+	}
+
+}
